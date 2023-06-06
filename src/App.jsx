0bc6111d52b303db254e60fb52e74b6ea18e4ad3
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-07 04:15:41 +0530

    components and props with validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+import './App.css';
+import PropTypes from 'prop-types';
 
 function App() {
 
@@ -8,8 +9,27 @@ function App() {
     <div>
       <h1>Hello World</h1>
       {name}
+      <User name="Sudarshan Shah" age={25} email="sudarshanshah911@gmail.com" />
+      <User name="Henry Cavil" age={40} email="henrycavil@gmail.com" />
     </div>
   )
+}
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>Name : {props.name}</h1>
+      <h2>Age : {props.age}</h2>
+      <h2>Email : {props.email}</h2>
+    </div>
+  )
+}
+
+// validation for props field in User component
+User.propTypes = {
+  name : PropTypes.string,
+  age : PropTypes.number,
+  email : PropTypes.string
 }
 
 export default App
