32fb9ef90f54dda9fd91c6d5b4cadee6aad522b5
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-07 03:36:33 +0530

    react-project-setup starter-template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  const name = <h1>Sudarshan</h1>;
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      {name}
+    </div>
+  )
+}
+
+export default App
