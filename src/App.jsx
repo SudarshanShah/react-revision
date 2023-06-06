5415a2d1df30405d4ffc4b08d55a88c506c36563
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-07 04:32:35 +0530

    conditional rendering using ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,17 @@ function App() {
 
   const name = <h1>Sudarshan</h1>;
 
+  const age = 25;
+
   return (
     <div>
       <h1>Hello World</h1>
       {name}
       <User name="Sudarshan Shah" age={25} email="sudarshanshah911@gmail.com" />
       <User name="Henry Cavil" age={40} email="henrycavil@gmail.com" />
+
+      {/* Conditional rendering */}
+      {age >= 18 ? <h1>Eligible to Vote</h1> : <h1>You are not an Adult</h1>}
     </div>
   )
 }
